a371dfb62e88fa6572ddad46bfe44256289e8b75
Mudasir Ali <mudasir.ali@axelerant.com>
2024-08-07 10:08:13 +0530

    Learning JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
-function App() {
+import React from "react";
 
-  return 'Hello World!'
+function App() {
+  // return React.createElement("h1", { id: "5" }, "Hello world");
+  // return <h1>Hello World</h1>;
+  return (
+    <div className="large" id="largerDiv">
+      <label htmlFor="inputId">label text</label>
+      <input id="inputId" type="number" value={3}></input>
+    </div>
+  );
 }
 
-export default App
+export default App;
