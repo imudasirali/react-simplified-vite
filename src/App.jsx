330aa5a0c1b82f663c0953b5ffbde5ddb2009e7c
Mudasir Ali <mudasir.ali@axelerant.com>
2024-08-09 21:12:52 +0530

    Practiced samll component project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
-function App() {
+import CardFunction from "./components/CardFunction";
+import CardClass from "./components/CardClass";
+import user from "./json/user.json";
+import "./css/style.css";
 
-  return 'Hello World!'
+function App() {
+  return (
+    <div>
+      <CardFunction
+        name={user.name}
+        age={user.age}
+        phoneNumber={user.phoneNumber}
+        address={user.address}
+      ></CardFunction>
+
+      <br />
+
+      <CardClass
+        name={user.name}
+        age={user.age}
+        phoneNumber={user.phoneNumber}
+        address={user.address}
+      ></CardClass>
+    </div>
+  );
 }
 
-export default App
+export default App;
