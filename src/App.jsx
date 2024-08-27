fd710a65d864602952f48fac6ec4e99636a236de
Mudasir Ali <mudasir.ali@axelerant.com>
2024-08-27 11:48:41 +0530

    LDT-552: Completed useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
-function App() {
+import { useState, useRef, useEffect } from "react";
 
-  return 'Hello World!'
+function App() {
+  const [name, setName] = useState("");
+  const inputRef = useRef("");
+  useEffect(() => {
+    console.log("rendered");
+  }, [name]);
+
+  function generateValue() {
+    inputRef.current = Math.random();
+    console.log(inputRef.current);
+    // setName((currentName) => inputRef.current);
+  }
+
+  return (
+    <>
+      <label>
+        Name:
+        <input
+          value={inputRef.current}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <button onClick={generateValue}>Generate Value</button>
+    </>
+  );
 }
 
-export default App
+export default App;
