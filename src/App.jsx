a34126681cb204f309cd8e12578aa5c05fbe5db5
Mudasir Ali <mudasir.ali@axelerant.com>
2024-10-15 20:35:05 +0530

    LDT-555: basic routing in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,41 @@
-function App() {
+import { About } from "./pages/About";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
+import "./styles.css";
 
-  return 'Hello World!'
+function App() {
+  const url = window.location.pathname;
+  let page = "";
+  switch (url) {
+    case "/about":
+      page = <About />;
+      break;
+    case "/store":
+      page = <Store />;
+      break;
+    default:
+      page = <Home />;
+      break;
+  }
+
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/store">Store</a>
+          </li>
+        </ul>
+      </nav>
+      <main>{page}</main>
+    </>
+  );
 }
 
-export default App
+export default App;
